926a0408a6b8e5db32c32245477916ce603c2541
murmurhash32 <94634972+murmurhash32@users.noreply.github.com>
2021-11-20 22:23:22 +0200

    .01

--- main.py
+++ main.py
@@ -1,30 +1,32 @@
 from tkinter import *
 
-Black = "#000000"
 
-master = Tk()
-canvas_width = 200
-canvas_height = 200 
-c = Canvas(master, 
-			width=canvas_width,
-			height=canvas_height)
-c.pack()
 
 
 def grided(canvas, line_distance):
-	canvas.create_line(10, 0, 10, 10, fill=Black)
-	canvas.create_line(10, 10, 20, 10, fill=Black)
+	canvas.create_line(10, 0, 10, 10, fill=BLACK)
+	canvas.create_line(10, 10, 20, 10, fill=BLACK)
 
 	"""
 	for x in range(line_distance, canvas_width, line_distance):
-		canvas.create_line(x, 0, x, canvas_height, fill=Black)
+		canvas.create_line(x, 0, x, canvas_height, fill=BLACK)
 	for y in range(line_distance, canvas_height, line_distance):
-		canvas.create_line(0, y, canvas_width, y, fill=Black)
+		canvas.create_line(0, y, canvas_width, y, fill=BLACK)
 	"""
 
 
 
 
-grided(c, 5)
+if __name__ == "__main__":
+	BLACK = "#000000"
 
-mainloop()
+	master = Tk()
+	canvas_width = 200
+	canvas_height = 200 
+	c = Canvas(master, 
+				width=canvas_width,
+				height=canvas_height)
+	c.pack()
+	
+	grided(c, 5)
+	mainloop()
